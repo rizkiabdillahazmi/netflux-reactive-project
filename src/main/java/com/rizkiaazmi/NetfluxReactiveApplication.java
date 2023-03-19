0a4912a697a256e6078109a333338d84bde52937
Rizki Abdillah Azmi <rizki.abdillah@mobbi.id>
2023-03-19 14:17:33 +0700

    Initial Commit

--- src/main/java/com/rizkiaazmi/NetfluxReactiveApplication.java
+++ src/main/java/com/rizkiaazmi/NetfluxReactiveApplication.java
@@ -1,4 +1,4 @@
-package com.example.netfluxreactive;
+package com.rizkiaazmi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/rizkiaazmi/NetfluxReactiveApplicationTests.java
+++ src/test/java/com/rizkiaazmi/NetfluxReactiveApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.netfluxreactive;
+package com.rizkiaazmi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
