6c3871b863039b6f22c523cb50a370d121a568f2
Rizki Abdillah Azmi <rizki.abdillah@mobbi.id>
2023-03-19 14:16:05 +0700

    Initial Commit

---
+++ src/test/java/com/rizkiaazmi/NetfluxReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.netfluxreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetfluxReactiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
